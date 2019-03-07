ab0d8b990ffabf9cc16c044f585259737859e855
Dutong <du.to@husky.neu.edu>
2019-03-06 17:18:33 -0800

    Assignment7+Assignment8

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ArticleList articles={articles} />  
+      <ArticleList articles={articles} />  
       </div>
     );
   }
